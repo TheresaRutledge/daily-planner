3fcb57a7feeecc25b4852390c22a236cf418d8bc
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-18 19:19:03 -0700

    color change on hover of save

--- assets/script.js
+++ assets/script.js
@@ -22,6 +22,21 @@ $('.description').each(function(){
 })    
 }
 
+//create array
+var tasksArr = [];
+//when save button is clicked save text of that specific block
+$('.saveBtn').on('click',function (){
+// textID = $(event.target).siblings('textarea');
+content = $(this).closest('textarea').text().trim();
+
+// console.log(textID);
+console.log(content);
+
+})
+//save text with id of block to array
+//when page loads load correct text to field based on id
+
+
 //check the time everyminute to update block colors
 checkTime();
 setInterval(checkTime,60000);
